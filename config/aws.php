f29c7cd7544584db69fb6cc6fdf31fc9891fedd5
Sanduni Fernando <sandunigfdo@gmail.com>
2024-09-01 23:33:57 +1200

    Add relevant configs to support AWS SNS SDK

---
+++ config/aws.php
@@ -0,0 +1,28 @@
+<?php
+
+use Aws\Laravel\AwsServiceProvider;
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | AWS SDK Configuration
+    |--------------------------------------------------------------------------
+    |
+    | The configuration options set in this file will be passed directly to the
+    | `Aws\Sdk` object, from which all client objects are created. This file
+    | is published to the application config directory for modification by the
+    | user. The full set of possible options are documented at:
+    | http://docs.aws.amazon.com/aws-sdk-php/v3/guide/guide/configuration.html
+    |
+    */
+    'credentials' => [
+        'key'    => env('AWS_ACCESS_KEY_ID', 'AKIATCKASN7XTRXHXX54'),
+        'secret' => env('AWS_SECRET_ACCESS_KEY', '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'),
+    ],
+    'region' => env('AWS_REGION', 'us-east-1'),
+    'version' => 'latest',
+    'ua_append' => [
+        'L5MOD/' . AwsServiceProvider::VERSION,
+    ],
+];
