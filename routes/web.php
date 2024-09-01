6a7fa65ec9cd362635ca917cb02f3f48761d466b
Sanduni Fernando <sandunigfdo@gmail.com>
2024-09-01 23:41:32 +1200

    Create routes

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,19 @@
 <?php
 
+use App\Actions\SendNotificationsAction;
+use App\Http\Controllers\NotificationsController;
+use App\Http\Controllers\UserCreatedWebhookController;
+use App\Http\Controllers\WeatherController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/weather', [WeatherController::class, 'getWeatherData'])->name('weather');
+
+Route::get('/test', [SendNotificationsAction::class, 'execute'])->name('test');
+
+Route::post('/user_created', [UserCreatedWebhookController::class, 'execute'])->name('user_created');
+
+Route::post('/send_message', [NotificationsController::class, 'sendNotification'])->name('send_message');
