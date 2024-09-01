34a2131038a6c732ae2273cd47a3432574e4794d
Sanduni Fernando <sandunigfdo@gmail.com>
2024-09-01 23:39:41 +1200

    Add user_created endpoint to CSRF exlude list

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -11,7 +11,9 @@ return Application::configure(basePath: dirname(__DIR__))
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware) {
-        //
+        $middleware->validateCsrfTokens(except: [
+            '/user_created',
+        ]);
     })
     ->withExceptions(function (Exceptions $exceptions) {
         //
