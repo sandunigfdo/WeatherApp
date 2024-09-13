acc3871aa259c84d32ef6444bd585ec06f42843c
Sanduni Fernando <sandunigfdo@gmail.com>
2024-09-14 10:54:58 +1200

    Exclude topic delete csrf validation

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -13,6 +13,7 @@ return Application::configure(basePath: dirname(__DIR__))
     ->withMiddleware(function (Middleware $middleware) {
         $middleware->validateCsrfTokens(except: [
             '/user-created-webhook',
+            '/topics/*',
         ]);
     })
     ->withExceptions(function (Exceptions $exceptions) {
