63e5babafb2f67aa904dd8edf329bd2ef9434510
Sanduni Fernando <sandunigfdo@gmail.com>
2024-09-14 00:36:08 +1200

    Cleanup

--- resources/views/topics.blade.php
+++ resources/views/topics.blade.php
@@ -894,12 +894,6 @@
                             <h1 class="text-base font-semibold leading-6 text-gray-900">Topics</h1>
                             <p class="mt-2 text-sm text-gray-700">A list of all topics in the storage NOT IN AWS.</p>
                         </div>
-                        <div class="mt-4 sm:ml-16 sm:mt-0 sm:flex-none">
-                            <button type="button"
-                                    class="block rounded-md bg-indigo-600 px-3 py-2 text-center text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">
-                                Clear all topics
-                            </button>
-                        </div>
                     </div>
                     <div class="mt-8 flow-root">
                         <div class="-mx-4 -my-2 overflow-x-auto sm:-mx-6 lg:-mx-8">
